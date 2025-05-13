a2cc95f62a38c7704dbb00aba9bca7f4254ffdea
kimnari1073 <nari1073@naver.com>
2025-05-13 19:51:02 +0900

    init: Spring boot 프로젝트 추가

---
+++ backend/approval/src/main/java/com/example/approval/controller/DraftController.java
@@ -0,0 +1,23 @@
+package com.example.approval.controller;
+
+import com.example.approval.service.DraftService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/draft")
+public class DraftController {
+
+    @Autowired
+    private DraftService draftService;
+
+    @PostMapping
+    public ResponseEntity<Void> createDraft() {
+        draftService.
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ backend/approval/src/main/java/com/example/approval/service/DraftService.java
@@ -0,0 +1,10 @@
+package com.example.approval.service;
+
+import com.example.approval.dto.createDocOutputDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DraftService {
+
+    public createDocOutputDto
+}
